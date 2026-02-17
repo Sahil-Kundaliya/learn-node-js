f5f4c2336e8a2b4b7e15d15d827d4345045846d3
Sahil Kundaliya <sahilkundaliya.dev@gmail.com>
2026-02-17 17:57:29 +0530

    fs added

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const readline = require("readline");
+const readline = require("readline"); // used for reading input from the command line
+const fs = require("fs"); // used for file system operations, such as reading and writing files
 
-console.log("Hello World!");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
-rl.question("What is your name? ", (name) => {
-    console.log(`Hello, ${name}!`);
-    rl.close();
-});
+// A
+// console.log("Hello World!");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-rl.on("close", () => {
-    console.log("Interface closed");
-    process.exit(0);
-});
+// rl.question("What is your name? ", (name) => {
+//     console.log(`Hello, ${name}!`);
+//     rl.close();
+// });
+
+// rl.on("close", () => {
+//     console.log("Interface closed");
+//     process.exit(0);
+// });
+
+// B
+const fileContent = fs.readFileSync('./files/input.txt', 'utf-8'); // reads the content of the file synchronously and returns it as a string
+console.log(fileContent); // prints the content of the file to the console
+const modifiedContent = `Modified: ${fileContent} at time \n ${new Date().toLocaleString()}`; // modifies the content of the file by adding a prefix
+
+fs.writeFileSync('./files/output.txt', modifiedContent); // writes the content of the file to a new file called output.txt synchronously
+console.log("File has been written successfully!"); // prints a success message to the console
