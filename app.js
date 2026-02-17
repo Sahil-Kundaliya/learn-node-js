446624090a4cddae4754f16e95646bad1c4e1302
Sahil Kundaliya <sahilkundaliya.dev@gmail.com>
2026-02-17 18:16:51 +0530

    added readFile

--- app.js
+++ app.js
@@ -20,9 +20,36 @@ const fs = require("fs"); // used for file system operations, such as reading an
 // });
 
 // B
-const fileContent = fs.readFileSync('./files/input.txt', 'utf-8'); // reads the content of the file synchronously and returns it as a string
+/* const fileContent = fs.readFileSync('./files/input.txt', 'utf-8'); // reads the content of the file synchronously and returns it as a string
 console.log(fileContent); // prints the content of the file to the console
 const modifiedContent = `Modified: ${fileContent} at time \n ${new Date().toLocaleString()}`; // modifies the content of the file by adding a prefix
 
 fs.writeFileSync('./files/output.txt', modifiedContent); // writes the content of the file to a new file called output.txt synchronously
-console.log("File has been written successfully!"); // prints a success message to the console
+console.log("File has been written successfully!"); // prints a success message to the console */
+
+// C
+fs/* .readFile('./files/start.txt', 'utf-8', (err, data) => { // reads the content of the file asynchronously and returns it as a string
+    if (err) {
+        console.error("Error reading file:", err); // logs an error message if there is an error reading the file
+        return;
+    }
+    console.log(data);
+
+    fs.readFile(`./files/${data}.txt`, 'utf-8', (err1, data1) => { // reads the content of the file asynchronously and returns it as a string
+        if (err1) {
+            console.error("Error reading file:", err1); // logs an error message if there is an error reading the file
+            return;
+        }
+        console.log(data1);
+
+        fs.writeFile('./files/final.txt', `${data}\n\n${data1}`, (err2) => { // writes the content of the file to a new file called final.txt asynchronously
+            if (err2) {
+                console.error("Error writing file:", err2); // logs an error message if there is an error writing the file
+                return;
+            }
+            console.log("File has been written successfully!"); // prints a success message to the console
+        });
+    });
+}); // prints the content of the file to the console
+
+console.log("This will be printed before the file content is read."); */ // demonstrates that the file reading is asynchronous by printing this message before the file content is read
